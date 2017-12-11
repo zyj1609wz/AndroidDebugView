e82b6fe175d87f36c54661835f7123ddb67da36c
zhaoyanjun <362299465@qq.com>
2017-12-11 18:35:27 +0800

    第一次提交

--- app/src/main/java/com/debug/MainActivity.java
+++ app/src/main/java/com/debug/MainActivity.java
@@ -2,12 +2,26 @@ package com.debug;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements android.view.View.OnClickListener{
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.tv);
+
+        String s = "";
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
